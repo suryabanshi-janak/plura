fd911b2bddecd5532a8f6089d2ad8bc1683dd320
Janak Suryabanshi <suryabanshi.jnk@gmail.com>
2024-04-21 12:44:38 +0545

    setting clerk, uploadthing up

---
+++ src/app/(main)/agency/(auth)/layout.tsx
@@ -0,0 +1,7 @@
+import { PropsWithChildren } from 'react';
+
+export default function AuthLayout({ children }: PropsWithChildren) {
+  return (
+    <div className='h-full flex items-center justify-center'>{children}</div>
+  );
+}
